a857c831e87b581168be313af7302309eaef9b3b
HernanFerreyra <hernan8.ferreyra@gmail.com>
2021-11-02 20:10:05 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+//import './App.css';
+import './App2.scss';
+
+function App() {
+  return (
+    <div className="my_clase">
+        Hola Todos!!!
+    </div>
+  );
+}
+
+export default App;
